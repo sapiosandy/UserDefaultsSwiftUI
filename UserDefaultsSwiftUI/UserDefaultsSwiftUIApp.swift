d6c8e4cd4b15b5e53c3b1b6874c78ed0f967bd3c
Sandy Gomez <sgomez1029@gmail.com>
2024-04-16 12:32:37 -0700

    Initial Commit

---
+++ UserDefaultsSwiftUI/UserDefaultsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsSwiftUIApp.swift
+//  UserDefaultsSwiftUI
+//
+//  Created by Sandra Gomez on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
